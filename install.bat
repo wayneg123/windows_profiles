73cc55e77e2b3d4b813bcece42765d8f7325ad81
wayneg123 <2211403035@stmail.ujs.edu.cn>
2017-05-18 15:52:05 +0800

    modified install.bat

--- install.bat
+++ install.bat
@@ -1,10 +1,18 @@
 :: start from atom
-mklink /H %home%/.atom/.apmrc atom/.apmrc
-mklink /H %home%/.atom/config.cson atom/.config.cson
-mklink /H %home%/.atom/keymap.cson atom/keymap.cson
-mklink /H %home%/.atom/projects.cson atom/projects.cson
+mklink /H %home%\.atom\.apmrc atom\.apmrc
+mklink /H %home%\.atom\config.cson atom\.config.cson
+mklink /H %home%\.atom\keymap.cson atom\keymap.cson
+mklink /H %home%\.atom\projects.cson atom\projects.cson
 :: pip
 cd %home%
 mkdir pip
-cd %home%/windows_profile
-mklink /H %home%/pip/pip.ini pip/pip.ini
+cd %home%\windows_profile
+mklink /H %home%\pip\pip.ini pip\pip.ini
+:: cmder
+mklink /H %home%\cmder\ConEmu.xml cmder\ConEmu.xml
+mklink /H %home%\cmder\ cmder\
+mklink /H %home%\cmder\ cmder\
+mklink /H %home%\cmder\ cmder\
+:: gvim
+mklink /H %home%\_vimrc gvim\_vimrc
+
